8a61e11fd5b81dfb8837aa1951fc0767b678aa50
Greg Beam <ki7mt@yahoo.com>
2016-06-20 17:10:22 -0600

    Add simple epoch date and time conversion functions

--- wsprana/utils.py
+++ wsprana/utils.py
@@ -5,6 +5,7 @@ General utilities module for WSPR-Ana
 """
 
 import csv
+import datetime
 
 #--------------------------------------------------------- Get CSV Column Count
 def csv_column_count(in_file):
@@ -21,7 +22,7 @@ def csv_column_count(in_file):
 
     Returns
     -------
-    ccount --- Returned number of columns in csv file
+    ccount -- number of columns in csv file (in_file)
 
     """
     try:
@@ -48,7 +49,7 @@ def csv_record_count(in_file):
 
     Returns
     -------
-    rcount --- Returned number of columns in csv file
+    rcount --number of records in csv file (in_file)
 
     """
     try:
@@ -74,12 +75,49 @@ def enter_callsign():
 
     Returns
     -------
-    call -- in upper case
+    call in upper case
 
     """
     call = input("Enter callsign: ")
     call = call.upper()
     return call
+
+#----------------------------------------------------------- Convert Epoch Date
+def convert_epoch_date(ecpch_time_stamp)
+    """Converts unix epoch time in seconds to date
+    
+    Parameters
+    ----------
+    Epoch time in seconds
+    
+    Returns
+    -------
+    utc_date: %Y-%m-%d
+
+    """
+    timestamp = (ecpch_time_stamp)
+    initial_value = datetime.datetime.fromtimestamp(ecpch_time_stamp)
+    utc_date = (value.strftime('%Y-%m-%d'))
+    return utc_date
+
+#----------------------------------------------------------- Convert Epoch Time
+def convert_epoch_time(ecpch_time_stamp)
+    r"""Convert epoch time in seconds to time
+    
+    Parameters
+    ----------
+    Epoch time in seconds
+    
+    Returns
+    -------
+    utc_date: %H%M
+
+    """
+    timestamp = (ecpch_time_stamp)
+    initial_value = datetime.datetime.fromtimestamp(ecpch_time_stamp)
+    utc_time = (value.strftime('%H%M'))
+    return utc_time
+
 
 ###############################################################################
 # GENERAL ERROR MESSAGES
@@ -92,8 +130,4 @@ def file_io_error_msg(io_error):
     print(io_error)
     print("Check file exists, path is correct, and file is not currupt.")
     print("")
-
-
-
-
 
