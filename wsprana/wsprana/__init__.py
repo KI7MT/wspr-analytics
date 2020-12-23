35037e36141f3d256b15799a2c84fb971ee5890f
KI7MT <ki7mt@yahoo.com>
2020-12-23 16:36:11 -0700

    Fix build sdist by add wheel to dev requirments

--- wsprana/wsprana/__init__.py
+++ wsprana/wsprana/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.1.0"
-__title__ = 'WSPR Analytics'
+__title__ = 'wsprana'
 __author__ = 'Greg Beam, KI7MT'
 __license__ = 'Apache License, Version 2.0'
 __email__ = 'ki7mt@yahoo.com'
