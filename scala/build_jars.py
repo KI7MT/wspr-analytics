14aaf873801c924e1b5889237ef75ad16d74bb37
KI7MT <ki7mt@yahoo.com>
2020-12-28 05:51:40 -0700

    Add Copy Jar section to build_jar script

--- scala/build_jars.py
+++ scala/build_jars.py
@@ -1,21 +1,20 @@
-"""
-    Build Project Scala and Java Jar Files
-    Original Source: https://github.com/databricks/LearningSparkV2
-"""
 import os
-import sys
+import glob
+import shutil
 
 import multiprocessing
 from multiprocessing import Pool
 
 cpu = multiprocessing.cpu_count() - 1
+jars_dir = os.getcwd() + "/jars"
+target_dir = 'target/scala-2.12'
 
 def build_jar_files(dir):
     """
     Build a list of directories via SBT    
     """
-    print(f"\n* Building Jar files {dir}\n")
-    cmd = f"cd {dir}/ && sbt clean package"
+    print(f"* Building Jar files {dir}")
+    cmd = f"cd {dir}/ && sbt clean assembly"
     
     exit_status = os.system(cmd)
     
@@ -24,11 +23,23 @@ def build_jar_files(dir):
     return exit_status
 
 if __name__ == '__main__':
+    
     # List of folders to build
-    folders = ["TopTenReporters"]
+    folders = ['ConvertCsvToParquet', 'QueryColumnParquet']
     with Pool(cpu) as p:
         p.map(build_jar_files, folders)
     
     p.close()
     p.terminate()
+
+    print("\nCopy All Jar Assemblys")
+    for f in folders:
+        file_path = f+"/"+target_dir+"/"
+        files = glob.iglob(os.path.join(file_path, "*.jar"))
+        for file in files:
+            if os.path.isfile(file):
+                print(f"* {file}")
+                shutil.copy2(file, jars_dir)
+
+
     print("\n* Finished\n")
