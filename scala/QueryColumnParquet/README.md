fca10becc23c7593a2cc4e16b63216984a646541
KI7MT <ki7mt@yahoo.com>
2020-12-28 07:53:40 -0700

    Update the docs and main scala file

--- scala/QueryColumnParquet/README.md
+++ scala/QueryColumnParquet/README.md
@@ -1,16 +1,10 @@
-# Build Process
+# Query Column Using Parquet Files
 
 This is a sample Application using [Scala][] that performs the following:
 
-* Reads the Original CSV into a Spark DataFrame
-* Creates a Parquet file set
-* Performas a Query Count on Reporters Ordered Descending
-* Reports Top (10) by spot count
-* The Parquet compression is set to default => "snappy"
-
-If you re-run the script, you need to remove the previous
-directory beforehand as `df.wite.parquet()` will `not` overwrite
-existing data.
+* Reads a Parquet folder specificed by the user e.g. /data/wspr/raw/parquet/2020/02
+* Performs a Query Count on a column specified by the user e.g. Reporter
+* Reports GoubyBy Count and returns the Top (10) 
 
 ## Framework Requirements
 
@@ -44,58 +38,47 @@ Run the following commands in order, and check your results.
 # All commands are run from a terminal
 #
 
-# change the download location to whatever you prefer
-cd ~/Downloads
-wget -c http://wsprnet.org/archive/wsprspots-2020-02.csv.gz
-gzip -dk wsprspots-2020-02.csv.gz
+# NOTE: You must have previously run ConvertCvsToParquet before
+#       useing this app, as it is looking for a Parquet folder,
+#       not a CSV file.
 
-# set the path of the downloaded and extracted CSV file
-csvfile=$PWD/wsprspots-2020-02.csv
+# set the path to where you created the Parquet partition
+inFolder="/data/wspr/raw/parquet/2020/02"
+column="Reporter"
 
 # clone the repo
 git clone https://github.com/KI7MT/wspr-analytics.git
 
 # change directories and build the assembly
-cd ./wspr-analytics/scala/ConvertCsvParquet
+cd ./wspr-analytics/scala/QueryColumnParquet
 
 # clean and build
 sbt clean assembly
 
-# Ensure the output directory is free from previous runs
-# Change 2020 and 02 to the year and month of CSV file being tested
-rm -rf /tmp/wsprspots/2020/02
-
 # Run the following command
 # NOTE : set local[8] to half of your total CPU count. 
-spark-submit --master local[8] target/scala-2.12/ConvertCsvToParquet-assembly-1.0.jar $csvfile
+spark-submit --master local[8] target/scala-2.12/QueryColumnParquet-assembly-1.0.jar $inFolder $column
 ```
 
-### Results
+## Results
 
 You should get results similar to the following:
 
 >NOTE The time it takes will depend on your system resources (CPU, RAM, etc)
 
 ```bash
-Application   : ConvertCsvToParquet
-Process File  : wsprspots-2020-02.csv
-File Out Path : /tmp/wsprspots/2020/02
-Tiimestame    : 2020-12-27 T 02:45:14.346
-Description   : Convert CSV to Parquet and Query Reporters
+Application   : QueryColumnParquet
+Folder        : /data/wspr/raw/parquet/2020/02
+Column        : Reporter
+Tiimestame    : 2020-12-28T07:36:35.389
+Description   : Query Column and Count using Parquet Folders
 
-Process Steps to Create Parquet File(s)
+Process Steps to Query Reporter from Parquet Files(s)
 - Create a Spark Session
-- Add The Spot Schema
-- Read The CSV into a DataSet
-- Write Parquet File(s), please wait...
-
-Elapsed Time : 30.836 sec
-
-Process Steps to Query Reporters from Parquet Files(s)
 - Read Parquet File(s)
-- Select Reporters
-- GroupBy and Count Reporters
-- Sort Reporters Descending
+- Select Reporter
+- GroupBy and Count Reporter
+- Sort Reporter Descending
 - Execute the Query
 
 +--------+------+
@@ -114,8 +97,122 @@ Process Steps to Query Reporters from Parquet Files(s)
 +--------+------+
 only showing top 10 rows
 
-Elapsed Time : 1.769 sec
+Elapsed Time : 2.843 sec
 ```
+## Query Reporter for November 2020
+
+For Comparrison, here is the data from `2020-11`. November 2020 had over **70 Million** reports.
+
+```bash
+Application   : QueryColumnParquet
+Folder        : /data/wspr/raw/parquet/2020/11
+Column        : Reporter
+Tiimestame    : 2020-12-28 T 07:42:09.275
+Description   : Query Column and Count using Parquet Folders
+
+Process Steps to Query Reporter from Parquet Files(s)
+- Create a Spark Session
+- Read Parquet File(s)
+- Select Reporter
+- GroupBy and Count Reporter
+- Sort Reporter Descending
+- Execute the Query
+
++--------+-------+
+|Reporter|  count|
++--------+-------+
+|  EA8BFK|1120739|
+|  OE9GHV|1103335|
+|   WA2TP| 847124|
+|   KD2OM| 834896|
+|  IW2NKE| 818315|
+|   LX1DQ| 803347|
+|   DK6UG| 794675|
+|KA7OEI-1| 748356|
+|   ON5KQ| 744727|
+|  OE9HLH| 733580|
++--------+-------+
+only showing top 10 rows
+
+Elapsed Time : 2.8 sec
+
+```
+
+## Query CallSign for November 2020
+
+This query is from the `CallSign` column.
+
+```bash
+Application   : QueryColumnParquet
+Folder        : /data/wspr/raw/parquet/2020/11
+Column        : CallSign
+Tiimestame    : 2020-12-28 T 07:44:16.711
+Description   : Query Column and Count using Parquet Folders
+
+Process Steps to Query CallSign from Parquet Files(s)
+- Create a Spark Session
+- Read Parquet File(s)
+- Select CallSign
+- GroupBy and Count CallSign
+- Sort CallSign Descending
+- Execute the Query
+
++--------+------+
+|CallSign| count|
++--------+------+
+|   DK2DB|662784|
+|   K4APC|600895|
+|   ON7KO|557477|
+|   KD6RF|541418|
+|  WA4KFZ|531006|
+|   W6LVP|466346|
+|   DL6NL|438741|
+|   N8VIM|438260|
+|   DK8JP|403667|
+|   G0CCL|386570|
++--------+------+
+only showing top 10 rows
+
+Elapsed Time : 2.445 sec
+
+```
+## Query Version for November 2020
+
+```bash
+Application   : QueryColumnParquet
+Folder        : /data/wspr/raw/parquet/2020/11
+Column        : Version
+Tiimestame    : 2020-12-28 T 07:50:17.725
+Description   : Query Column and Count using Parquet Folders
+
+Process Steps to Query Version from Parquet Files(s)
+- Create a Spark Session
+- Read Parquet File(s)
+- Select Version
+- GroupBy and Count Version
+- Sort Version Descending
+- Execute the Query
+
++---------+--------+
+|  Version|   count|
++---------+--------+
+|     null|27015499|
+| 1.3 Kiwi|13207118|
+|    2.2.2|11199415|
+|    2.1.2| 5906973|
+|2.3.0-rc1| 2952306|
+|    2.1.0| 2602903|
+|2.3.0-rc2| 2366520|
+|0.9_r4178| 2033622|
+|    2.2.1| 2027954|
+|    2.0.0| 1482597|
++---------+--------+
+only showing top 10 rows
+
+Elapsed Time : 2.492 sec
+
+```
+
 
 [wpsrspots-2020-02.csv.zip]: http://wsprnet.org/archive/wsprspots-2020-02.csv.zip
 [sdkman]: https://sdkman.io/
--- scala/QueryColumnParquet/src/main/scala/io/ki7mt/spark/QueryColumnParquet.scala
+++ scala/QueryColumnParquet/src/main/scala/io/ki7mt/spark/QueryColumnParquet.scala
@@ -11,33 +11,12 @@ import org.apache.spark.sql.functions._
 // Get The Top Ten Reporters by Count
 object QueryColumnParquet {
 
-  case class Spot(SpotId: Integer, 
-                  TimeStamp: Integer,
-                  Reporter: String,
-                  RxGrid: String,
-                  SNR: Integer,
-                  Frequency: Double,
-                  CallSign: String,
-                  Grid: String,
-                  Power: Integer,
-                  Drift: Integer,
-                  Distance: Integer,
-                  Azimuth: Integer,
-                  Band: Integer,
-                  Version: String,
-                  Code: Integer
-                  )
-
-  // Changed ro Milliseconds as Nanoseconds is a bit to high in percision
-  // Also added the Long $et for better readability
-  // https://biercoff.com/easily-measuring-code-execution-time-in-scala/
   def time[R](block: => R): R = {
       val t0 = System.currentTimeMillis()
       val result = block  // call the block
       val t1 = System.currentTimeMillis()
       val elapsedTimeMsec: Float = (t1 - t0)
       val elapsedTime: Float = (elapsedTimeMsec / 1000)
-
       println(f"Elapsed Time : $elapsedTime sec\n")
       result
   }
@@ -45,94 +24,53 @@ object QueryColumnParquet {
   // The main entry point
   def main(args: Array[String]) {
 
-    if (args.length <= 0) {
-      println("\nInput File Error")
-      println("Usage: ConvertCsvToParquet-assembly-1.0.jar $csvfile")
-      println("Example: wsprspots-2020-02.csv")
-      System.exit(1)
-    }
-
-    val csvfile: String = args(0)
-    val appname: String = "ConvertCsvToParquet"
-    val timestamp: String = LocalDateTime.now().toString()
-    val description: String = "Convert CSV to Parquet and Query Reporters"
-  
-    // Setup the output path
-    val pathSplit : Array[String] = csvfile.split("/")
-    val fileName : String = pathSplit.last
-    val shortName: String = fileName.substring(0, fileName.lastIndexOf('.'))
-    val nameArray : Array[String] = shortName.split("-")
-
-    // set the output directory
-    // example: /tmp/wsprspots/2020/02
-    val outputDir : String = "/tmp/" + nameArray(0) + "/" + nameArray(1) + "/" + nameArray(2).replace(".csv", "")
-
-    println(s"\nApplication   : $appname")
-    println(s"Process File  : $csvfile" )
-    println(s"File Out Path : $outputDir")
-    println(s"Tiimestame    : $timestamp")
-    println(s"Description   : $description\n" )
-
     // Set the Java Log Level
     Logger.getLogger("org").setLevel(Level.ERROR)
 
+    // TODO: convert to scopt
+    if (args.length <= 1) {
+      println("\nInput Processing Error\n")
+      println("The conversion script rerquires atwo parameters\n")
+      println("Input Folder     : /data/wspr/raw/parquet/2020/02")
+      println("Column           : Reporter")
+      println("\nExample\n")
+      println("spark-submit --master local[8] target/scala-2.12/QueryColumnParquet-assembly-1.0.jar $inFolder $column\n")
+      System.exit(1)
+    }
+
+    val inFolder: String = args(0)
+    val column: String = args(1)
+    val appname: String = "QueryColumnParquet"
+    val timestamp: String = LocalDateTime.now().toString()
+    val description: String = "Query Column and Count using Parquet Folders"
+
+    println(s"\nApplication   : $appname")
+    println(s"Folder        : $inFolder" )
+    println(s"Column        : $column")
+    println(s"Tiimestame    : $timestamp")
+    println(s"Description   : $description\n" )
+
     // Spark Session: local[*] uses all cores on the local machine
-    println("Process Steps to Create Parquet File(s)")
+    println(s"Process Steps to Query $column from Parquet Files(s)")
     println("- Create a Spark Session")
     val spark = SparkSession
       .builder
-      .appName("ConvertCsvToParquet")
-      //.master("local[16]")
+      .appName(s"Query $column with Parquet")
       .getOrCreate()
 
-    // create the schema
-    println("- Add The Spot Schema")
-    val spotSchema = new StructType()
-      .add("SpotID", IntegerType, nullable = false)
-      .add("Timestamp", IntegerType, nullable = false)
-      .add("Reporter", StringType, nullable = false)
-      .add("RxGrid", StringType, nullable = false)
-      .add("SNR", IntegerType, nullable = false)
-      .add("Frequency", DoubleType, nullable = false)
-      .add("CallSign", StringType, nullable = false)
-      .add("Grid", StringType, nullable = false)
-      .add("Power", IntegerType, nullable = false)
-      .add("Drift", IntegerType, nullable = false)
-      .add("Distance", IntegerType, nullable = false)
-      .add("Azimuth", IntegerType, nullable = false)
-      .add("Band", IntegerType, nullable = false)
-      .add("Version", StringType, nullable = false)
-      .add("Code", IntegerType, nullable = true)
-
-    // Read the CSV into the DataSet
-    println("- Read The CSV into a DataSet")
-    import spark.implicits._
-    val ds = spark.read
-      .schema(spotSchema)
-      .csv(csvfile)
-      .as[Spot]
-
-    //println("\nPrint the Spot Schema")
-    //ds.printSchema()
-
-    println("- Write Parquet File(s), please wait...\n")
-    val df = ds.toDF()
-    time { df.write.parquet(outputDir) }
-
-    println("Process Steps to Query Reporters from Parquet Files(s)")
     println("- Read Parquet File(s)")
-    val ds1 = spark.read.parquet(outputDir)
+    val ds = spark.read.parquet(inFolder)
     
-    // Select only the reporter column as that's all we need
-    println("- Select Reporters")
-    val reporter = ds1.select("Reporter")
+    // Select only the column providerd by the user
+    println(s"- Select $column")
+    val selection = ds.select(column)
 
     // Group and count
-    println("- GroupBy and Count Reporters")
-    val rank = reporter.groupBy("Reporter").count()
+    println(s"- GroupBy and Count $column")
+    val rank = selection.groupBy(column).count().alias("Count")
 
     // Sort the resulting dataset by count column descending (default is assending)
-    println("- Sort Reporters Descending")
+    println(s"- Sort $column Descending")
     val sortedResults = rank.sort(desc("Count"))
 
     // Print results from the dataset
